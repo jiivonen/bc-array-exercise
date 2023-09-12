8baf97e8c32d05893394b2c6063e16c76134fd64
Joonas Iivonen <joonas.iivonen@bc.fi>
2023-09-12 15:37:19 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,36 @@
+// Task 1
+// Create an array called "names" and set it's items to be
+// David, Ann, Leia, Mathew and Mark.
+// Print the array.
+
+// Task 2
+// Print the second item of the array from the task 1.
+
+// Task 3
+// Print the length of the array.
+
+// Task 4
+// Print the last item of the array
+
+// Task 5
+// Add Peter as the last item to the array and print the whole array
+
+// Task 6
+// Print the array with spaces, not commas
+
+// Task 7
+// Replace Mathew in the array with Lisa and Abraham 
+
+// Task 8
+// Sort the array in the alphabetical order and print it
+
+// Task 9
+// Create an array called numbers and fill it with values 1, 7, 45, 32, 27, 86.
+// Sort the numbers from largest to smallest. Print them.
+
+// Task 10
+// Randomize the numbers array.
+
+// Task 11
+// Find the largest and smallest value from the numbers array using sort() function.
+// Print those values.
